0e13446a64e865e62f229dce2160b2a4c4afbff9
Erce Fırat DEMİRCAN <erce.demircan@hubx.co>
2026-01-07 22:24:24 +0300

    Remove

--- extract_pdf.py
+++
@@ -1,8 +0,0 @@
-import sys
-try:
-    from pypdf import PdfReader
-    reader = PdfReader(sys.argv[1])
-    for page in reader.pages:
-        print(page.extract_text())
-except ImportError:
-    print("pypdf not found")
--- extract_pdf_2.py
+++
@@ -1,8 +0,0 @@
-import sys
-try:
-    import PyPDF2
-    reader = PyPDF2.PdfReader(sys.argv[1])
-    for page in reader.pages:
-        print(page.extract_text())
-except ImportError:
-    print("PyPDF2 not found")
